3cc84f1ca56069d6e146d908f6f3a459f4d29891
Jesper Andersson <ajesper00@gmail.com>
2017-11-17 17:43:02 +0100

    Test

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string output = "";
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+                if(i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+                if (output == "")
+                {
+                    output = i.ToString();
+                }
+                Console.WriteLine(output);
+                System.Threading.Thread.Sleep(60);
+            }
+            Console.ReadLine();
+        }
+    }
+}
